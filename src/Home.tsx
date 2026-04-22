f4bc63f3f3a5705f8d1191e63b46b0e5059923ff
bella_arimateia <moreiraisabella999@gmail.com>
2026-04-21 22:02:56 -0300

    Home atualizada

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,19 +1,27 @@
 import { Link } from 'react-router'
 import styles from './App.module.css'
+import react from './assets/react.png'
 
 export function Home() {
   return (
     <div className={styles['home-container']}>
-      <h3 className={styles['home-title']}>PROJETO REACT</h3>
+      <div className={styles['home-card']}>
+        <img src={react} alt="Logo React" className={styles.foto} />
 
-      <div className={styles['button-group']}>
-        <Link to="/cadastro">
-          <button className={styles['home-button']}>Cadastro</button>
-        </Link>
+        <h1 className={styles['home-title']}>🚀 PROJETO REACT</h1>
+        <p className={styles['home-subtitle']}>
+          Bem-vindo ao nosso projeto. Escolha uma das opções abaixo para continuar.
+        </p>
 
-        <Link to="/descricao">
-          <button className={styles['home-button']}>Descrição</button>
-        </Link>
+        <div className={styles['button-group']}>
+          <Link to="/cadastro">
+            <button className={styles['home-button']}>Cadastro</button>
+          </Link>
+
+          <Link to="/descricao">
+            <button className={styles['home-button']}>Descrição</button>
+          </Link>
+        </div>
       </div>
     </div>
   )
